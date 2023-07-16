1658f2710064d91490f82246aa925c271ef1f666
Ezra Peisach <ezra.peisach@rcsb.org>
2023-07-16 09:59:39 -0400

    Improve diffrn_source OneDep cross checks

--- tests/test_diffrn_source.py
+++ tests/test_diffrn_source.py
@@ -6,6 +6,12 @@ def getSourceTypes(dictionary):
     return stypes
 
 @pytest.fixture(scope = "module")
+def getSourceSources(dictionary):
+    stypes = dictionary.getEnumListAltWithDetail('diffrn_source', 'source')
+    return stypes
+
+
+@pytest.fixture(scope = "module")
 def getSites(dictionary):
     sites = dictionary.getEnumListAlt('diffrn_source', 'pdbx_synchrotron_site')
     return sites
@@ -47,4 +53,13 @@ def test_beamlinesused(getSourceTypes):
     #    self.assertIn(bLine, beamlines)
     # Not ready to remove beamlines yet.
 
+def test_sourcetypes(getSourceTypes, getSourceSources):
+    """Tests if the type fo the source is listed"""
+    stypes = [v[0] for v in getSourceSources]
+
+    for s in getSourceTypes:
+        src = s[0]
+        st = s[1]
+        if st:
+            assert st in stypes, f"'{src}' type '{st}' not in source types"
 
